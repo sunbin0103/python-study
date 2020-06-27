1556c16924a36bd046ebcfefb4d632b66d85cae6
sunbin0103 <sunbin0103@naver.com>
2020-06-28 00:49:22 +0900

    Add test, quiz
    스터디 당일 실습 파일 : test
    복습 예제 : quiz

---
+++ 2week/test.py
@@ -0,0 +1,38 @@
+print(10==10 and 10!=5)
+print(10>5 or 10<3)
+print(10>5 and 10<3)
+
+print(10>5)
+print(not 10>5)
+
+print(13&9)
+
+print(7|3)
+
+print(10^2)
+
+print(4<<2)
+print(4>>2)
+
+
+a=10
+b=[1,2,3,4,5]
+
+print(a in b)
+print(a not in b)
+
+print(5==5.0)
+print(5 is 5.0)
+
+x=10
+y=10
+
+print(x is y)
+print(id(x))
+print(id(y))
+
+y=50
+print(id(x))
+print(id(y))
+
+print(x is not y)
