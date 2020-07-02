fe106cf05010b6d53ab6c03f67126489fd36302a
sunbin0103 <sunbin0103@naver.com>
2020-07-02 21:57:55 +0900

    Add 3weeks.py
    3주차 스터디 - 자료형

---
+++ 3week/3weeks.py
@@ -0,0 +1,18 @@
+print('Life is too short,\nyou need python')
+
+a = {'trpg':['coc','insane'],
+     'rpg':['메이플스토리','마비노기']}
+
+print(a['trpg'])
+
+arr = [1,2,3]
+print(arr)
+
+arr[2] = 4
+print(arr)
+
+tp = (1,2,3)
+print(tp)
+
+tp[2] = 4
+print(tp)
