9c7818ba13c290e9ec8b8ac66018d0982c346324
Muya1039ui <67033539+Muya1039ui@users.noreply.github.com>
2020-07-08 14:42:10 +0900

    Add files via upload

---
+++ 3week/Python 3weeks.py
@@ -0,0 +1,17 @@
+#1번 문제
+print("""동해물과 백두산이
+마르고 닳도록
+하느님이 보우하사
+우리나라 만세""")
+
+#2번 문제
+listdate=[1,2,3,[45,67,89]]
+print(listdate[3][0])
+print(listdate[-1])
+
+#3번 문제
+dictdate={'CoC':'초여명','inSANe':'TRPG클럽'}
+print(dictdate['CoC'])
+
+#4번 문제
+print(set([1,1,1,2,2,3,3,3,4,4,5]))
